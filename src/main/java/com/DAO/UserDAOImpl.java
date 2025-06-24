2d268e3e541a961162c1e6b9c0620b5b50494df3
SONU PANDEY <SONU PANDEY@github.com/the-pandey-ji>
2025-06-24 09:26:42 +0530

    register servlet created

--- src/main/java/com/DAO/UserDAOImpl.java
+++ src/main/java/com/DAO/UserDAOImpl.java
@@ -1,5 +1,46 @@
 package com.DAO;
 
+import java.sql.Connection;
+
+import com.entity.User;
+
 public class UserDAOImpl implements UserDAO{
+	
+	private Connection conn;
+
+	public UserDAOImpl(Connection conn) {
+		super();
+		this.conn = conn;
+	}
+
+	
+	public boolean userRegister(User us) {
+		
+		try {
+			String query = "insert into user(empn,username,qtrno,email,phone,password) values(?,?,?,?,?,?)";
+			java.sql.PreparedStatement pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, us.getEmpn());
+			pstmt.setString(2, us.getUsername());
+			pstmt.setString(3, us.getQtrno());
+			pstmt.setString(4, us.getEmail());
+			pstmt.setInt(5, us.getPhone());
+			pstmt.setString(6, us.getPassword());
+
+			int i = pstmt.executeUpdate();
+
+			if (i == 1) {
+				return true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		return false;
+	}
+	
+	
 
 }
